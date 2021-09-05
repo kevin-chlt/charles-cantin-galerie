9d9e854d0ffc173785a009fc8e893571328201ed
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-09-05 15:05:08 +0200

    dropdown responsive fonction ok & class active fonction ok

--- assets/js/dropdown-categorie.js
+++ assets/js/dropdown-categorie.js
@@ -1,20 +1,47 @@
+// Revert class at each click on categories button dropdown //
 const list = document.getElementById('category-list');
 const title = document.getElementById('category-title');
-
 let open = false;
 
 title.addEventListener('click', () => {
     if (!open) {
-        title.classList.add('title-open');
-        title.classList.remove('title-close');
-        list.classList.add('open');
-        list.classList.remove('close');
-        open = true;
-    } else {
-        title.classList.add('title-close')
-        title.classList.remove('title-open');
-        list.classList.add('close');
-        list.classList.remove('open');
-        open = false;
+        title.className = 'title-open';
+        list.className = 'open';
+        setIdOnCategory();
+        return open = true;
     }
+        title.className = 'title-close';
+        list.className = 'close';
+        return open = false;
 });
+
+
+// Put ID on category items //
+const setIdOnCategory = () => {
+    const listLength = list.children.length;
+    const allLi = list.children;
+
+    for(let i = 0; i < listLength; i++) {
+        allLi[i].setAttribute('id', 'category-'+ i);
+    }
+
+    createEvent(listLength);
+}
+
+
+// Create event listener for each category & add function to put the "active" CSS class to the category item clicked   //
+const createEvent = (listLength) => {
+    let categoryActiveIndex = 7;
+
+    for(let i = 0; i < listLength; i++) {
+        const category = document.getElementById('category-'+ i);
+
+        category.addEventListener('click', () => {
+            const oldCategorie = document.getElementById('category-'+ categoryActiveIndex);
+            oldCategorie.classList.remove('active-category');
+            category.className = 'active-category';
+            return categoryActiveIndex = i;
+        });
+    }
+}
+

0feaaca6c4fa77e21affdf659dd83584f3835dbe
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-09-05 22:40:15 +0200

    finish form check - stable

--- assets/js/check-data.js
+++ assets/js/check-data.js
@@ -4,40 +4,42 @@ const lastName = document.getElementById('lastname');
 const email = document.getElementById('email');
 const telephone = document.getElementById('phone');
 const message = document.getElementById('message');
+const rgpd = document.getElementById('rgpd');
 let errors = [];
 
 requirejs(['node_modules/validator/validator.min'], (validator) => {
     form.addEventListener('submit', (e) => {
-        if(validator.isEmpty(firstName.value) || !validator.isAlpha(firstName.value) ) {
-            e.preventDefault();
-            errors.push('Veuillez saisir un nom composé uniquement de lettres.') ;
+        if(validator.isEmpty(firstName.value) || !validator.isAlpha(firstName.value || firstName.value.length > 50) ) {
+            errors.push('Veuillez saisir un prénom composé uniquement de lettres.') ;
         }
-        if (validator.isEmpty(firstName.value) || !validator.isAlpha(lastName.value)){
-            e.preventDefault();
-            errors.push('Veuillez saisir un prénom composé uniquement de lettres.');
+        if (validator.isEmpty(lastName.value) || !validator.isAlpha(lastName.value) || lastName.value.length > 50){
+            errors.push('Veuillez saisir un nom composé uniquement de lettres.');
         }
         if(validator.isEmpty(email.value) || !validator.isEmail(email.value)) {
-            e.preventDefault();
             errors.push('Veuillez saisir une adresse email valide.');
         }
         if(!validator.isMobilePhone(telephone.value, 'fr-FR')) {
-            e.preventDefault();
             errors.push('Veuillez saisir un numéro de téléphone valide.')
         }
-        if(validator.isLength(message.value, [{min: 10, max: 2400}])){
-            validator.escape(message.value);
-        }else {
-            e.preventDefault();
+        if(!validator.isLength(message.value, {min: 10, max: 2400})){
             errors.push('Veuillez entrer un message valide.')
         }
-        displayErrors(errors);
+        if(rgpd.checked === false) {
+            errors.push('Vous devez accepter nos termes concernant vos données.')
+        }
+
+        if(errors.length !== 0) {
+            e.preventDefault()
+            cleanHelpText();
+            displayErrors(errors);
+        }
         return errors = [];
     });
 });
 
 
 //Create loop for display the errors in the help-text div //
-const helpText = document.getElementById('help-text');
+let helpText = document.getElementById('help-text');
 const displayErrors = (errors) => {
     if(errors.length !== 0) {
         helpText.style.display = 'flex';
@@ -49,3 +51,11 @@ const displayErrors = (errors) => {
     }
 }
 
+// Delete child of help-text div //
+const cleanHelpText = () => {
+    let helpTextLength = helpText.children.length;
+    for(let i = helpTextLength - 1; i >= 0; i--) {
+        helpText.children[i].remove();
+    }
+
+}

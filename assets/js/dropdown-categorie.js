43737f41c7b488f9e1f815bc635afbacd16556ba
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-09-05 15:18:37 +0200

    bugfix dropdown

--- assets/js/dropdown-categorie.js
+++ assets/js/dropdown-categorie.js
@@ -7,7 +7,6 @@ title.addEventListener('click', () => {
     if (!open) {
         title.className = 'title-open';
         list.className = 'open';
-        setIdOnCategory();
         return open = true;
     }
         title.className = 'title-close';
@@ -27,21 +26,22 @@ const setIdOnCategory = () => {
 
     createEvent(listLength);
 }
-
-
 // Create event listener for each category & add function to put the "active" CSS class to the category item clicked   //
+
+
 const createEvent = (listLength) => {
     let categoryActiveIndex = 7;
-
     for(let i = 0; i < listLength; i++) {
-        const category = document.getElementById('category-'+ i);
 
+        const category = document.getElementById('category-'+ i);
         category.addEventListener('click', () => {
-            const oldCategorie = document.getElementById('category-'+ categoryActiveIndex);
-            oldCategorie.classList.remove('active-category');
+            const oldCategory = document.getElementById('category-'+ categoryActiveIndex);
+            oldCategory.classList.remove('active-category');
             category.className = 'active-category';
             return categoryActiveIndex = i;
         });
+
     }
 }
+setIdOnCategory();
 

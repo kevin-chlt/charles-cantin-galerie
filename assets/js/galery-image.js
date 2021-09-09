a49c49124b510efdc8077171fb66103769ed30c6
Kevin Challit <kevin.challit@gmail.com>
2021-09-09 22:11:51 +0200

    refresh displayPictures function

--- assets/js/galery-image.js
+++ assets/js/galery-image.js
@@ -2,6 +2,7 @@ const galery = document.getElementById('container-images-1');
 const list = document.getElementById('category-list');
 const btnMoreContainer = document.getElementById('btn-more-container');
 const btnMore = document.getElementById('btn-more');
+
 let categoryActiveId = 8;
 let itemBegin = 0;
 let itemEnd = 6;
@@ -25,12 +26,11 @@ const createEvent = () => {
 createEvent();
 
 
-
 // Request API, sort the pictures and call the display function //
 const requestCategory = async (id, itemBegin, itemEnd) => {
     let response = await fetch (`https://api-charles-cantin.herokuapp.com/categories/${id}`);
     let data = await response.json();
-    const categoryPictures = data.image.filter((e) => e.category === parseInt(id))
+    const categoryPictures = data.image.filter((e) => e.category === parseInt(id)) // Filter the image where the relation "category" field is = to id //
     const sortedPictures = categoryPictures.sort((a, b) => a.id < b.id) // Sort the picture by id //
     const sixFirstPictures = sortedPictures.slice(itemBegin, itemEnd); // Take the 6th last added first pictures //
     displayPictures(sixFirstPictures);
@@ -44,14 +44,24 @@ const displayPictures = (pictures) => {
     if(itemBegin === 0){
         clearGalery();
     }
-    for(let i = 0; i < 6; i++) {
+    let length =  pictures.length < 6 ? 6 : pictures.length
+    for(let i = 0; i < length; i++) {
+        const figure = document.createElement('figure');
+        const figcaption = document.createElement('figcaption');
         const img = document.createElement('img');
+        figure.appendChild(img);
+        figure.appendChild(figcaption)
         if(pictures[i] !== undefined) {
-            img.src = `${encodeURI(pictures[i].image.url)}`;
-        } else {
-            img.src = 'assets/images/charles-cantin-accueil_background.png';
+            img.src = encodeURI(pictures[i].image.url);
+            figcaption.textContent = pictures[i].image.name;
+        } else if(itemBegin !== 0) {
+            break;
         }
-        galery.appendChild(img);
+        else {
+            img.src = 'assets/images/charles-cantin-accueil_background.png';
+            figcaption.textContent = 'Logo Entreprise'
+        }
+        galery.appendChild(figure);
     }
 }
 
@@ -66,7 +76,7 @@ const clearGalery = () => {
 
 // Check if the new array from API is bigger than the 6 div create by DisplayPicture() and display or not the div Btn-more"
 const displayBtnMore = (pictures) => {
-    if(pictures.length < galery.children.length) {
+    if(pictures.length <= galery.children.length) {
         btnMoreContainer.style.display = 'none';
         btnMore.removeEventListener('click', eventBtnMore);
     }

43bde95b0e2f6eddf88d1141a5759d29ccf36a31
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-09-11 00:25:42 +0200

    Test localstorage online

---
+++ assets/js/secure-message-page.js
@@ -0,0 +1,9 @@
+const token = localStorage.getItem('token');
+console.log(token)
+
+if (!token) {
+    //window.location.href = 'https://api-charles-cantin.herokuapp.com/index.html';
+} else {
+    // Requete fetch //
+}
+

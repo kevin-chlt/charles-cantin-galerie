691be72c619f892424bac1008294d7c25828231b
Kevin CHALLIT <kevin.challit@gmail.fr>
2021-09-20 13:11:13 +0200

    debug function full screen picture for tel users 2

--- assets/js/galery/full-screen-picture.js
+++ assets/js/galery/full-screen-picture.js
@@ -1,4 +1,4 @@
-// Create the modal when click on the image //
+// Create the modal when click on the pictures //
 window.addEventListener('click', (e) => {
     if(e.target.nodeName === 'IMG' && e.target.parentElement.nodeName === 'FIGURE') {
         const modal = document.createElement('div');
@@ -10,12 +10,24 @@ window.addEventListener('click', (e) => {
 
         modal.append(picture)
         document.body.append(modal);
-    }
-})
 
-// Delete the modal when click on the background modal //
+        if(window.matchMedia("max-width: 500px")) {
+            const closeBtn = document.createElement('span');
+            closeBtn.textContent = "X";
+            closeBtn.className = 'close-modal'
+            modal.append(closeBtn);
+        }
+    }
+});
+
+
+// Delete the modal when click on the background or click on the cross in responsive screen //
 window.addEventListener('click', (e) => {
     if(e.target.className === 'modal'){
         e.target.remove();
     }
+    if(e.target.className === 'close-modal') {
+        document.querySelector('.modal').remove()
+    }
 })
+
